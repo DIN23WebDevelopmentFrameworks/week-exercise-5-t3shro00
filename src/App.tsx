b23603b2776a9ec313ec5c1e0068d79a95631877
romanshrestha20 <stha.roman20@gmail.com>
2024-10-14 18:46:57 +0300

    Assignment Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import RecipeTagList from './components/RecipeTagList';
+import RecipeList from './components/RecipeList';
+import { IRecipe } from './components/interfaces';
 
-const App = () => {
+const App: React.FC = () => {
+  const [tags, setTags] = useState<string[]>([]);
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const [recipes, setRecipes] = useState<IRecipe[]>([]);
 
+  // Load tags when the component mounts
+  useEffect(() => {
+    fetch('https://dummyjson.com/recipes/tags')
+      .then((res) => res.json())
+      .then((data) => setTags(data));
+  }, []);
+
+  // Load recipes when a tag is selected
+  useEffect(() => {
+    if (selectedTag) {
+      fetch(`https://dummyjson.com/recipes/tag/${selectedTag}`)
+        .then((res) => res.json())
+        .then((data) => setRecipes(data.recipes));
+    }
+  }, [selectedTag]);
+
+  const handleSelectTag = (tag: string) => {
+    setSelectedTag(tag);
+  };
+
+  const handleBack = () => {
+    setSelectedTag(null);
+  };
 
   return (
     <div>
-        <h1>ACME Recipe O'Master</h1>
-        <div>Remove this and implement recipe tag list here. </div>
-        <ul>
-        <li>On start the application displays a list of recipe tags such as 'pasta', 'cookies' etc. The tag information is loaded from an API (https://dummyjson.com/recipes/tags)</li>
-        <li> The user can click on a tag and the application will then hide the tag list and display a list of recipes matching the selected tag. The recipe information for the clicked tag is loaded from an API (https://dummyjson.com/recipes/tag/Pizza).</li>
-        <li> User can also go back to the tag list. </li>
-        <li> Each receipe is displayed as box where recipe data such as ingredients and instructions are displayed</li>
-        </ul>
+      <h1>ACME Recipe O'Master</h1>
+      {selectedTag ? (
+        <>
+          <button onClick={handleBack}>Back</button>
+          <RecipeList recipes={recipes} />
+        </>
+      ) : (
+        <RecipeTagList tagList={tags} onSelectTag={handleSelectTag} />
+      )}
     </div>
   );
 };
